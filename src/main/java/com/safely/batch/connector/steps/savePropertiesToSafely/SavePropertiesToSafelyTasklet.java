0dbb9e107888663828b219e031a50efc2a51445d
josh75337 <josh75337@gmail.com>
2020-06-25 13:29:55 -0400

    first draft of statistics for the SAve Properties Step

--- src/main/java/com/safely/batch/connector/steps/savePropertiesToSafely/SavePropertiesToSafelyTasklet.java
+++ src/main/java/com/safely/batch/connector/steps/savePropertiesToSafely/SavePropertiesToSafelyTasklet.java
@@ -5,6 +5,8 @@ import com.safely.api.domain.Property;
 import com.safely.batch.connector.common.domain.safely.auth.JWTToken;
 import com.safely.batch.connector.common.services.safely.SafelyConnectorPropertiesService;
 import com.safely.batch.connector.steps.JobContext;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,24 +25,58 @@ public class SavePropertiesToSafelyTasklet implements Tasklet {
   @Autowired
   private SafelyConnectorPropertiesService propertiesService;
 
+  private static final String UPDATED = "UPDATED";
+  private static final String CREATED = "CREATED";
+  private static final String FAILED = "FAILED";
+  private static final String FAILED_IDS = "FAILED_IDS";
+  private static final String STEP_NAME = "SAVE_PROPERTIES_TO_SAFELY";
+
   public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext)
       throws Exception {
+
+    HashMap<String, Object> stepStatistics = new HashMap<>();
 
     Organization organization = jobContext.getOrganization();
     JWTToken token = jobContext.getSafelyToken();
 
     List<Property> newProperties = jobContext.getNewProperties();
+
+    List<String> failedIds = new ArrayList<>();
+
     log.info("Writing {} new properties for organization: {} - ({})", newProperties.size(),
         organization.getName(), organization.getEntityId());
+    int successfullyCreated = 0;
+
     for (Property property : newProperties) {
-      propertiesService.create(token.getIdToken(), property);
+      try{
+        propertiesService.create(token.getIdToken(), property);
+        successfullyCreated++;
+      } catch(Exception e){
+        log.error("Failed to create property with ReferenceID {}", property.getReferenceId());
+        failedIds.add(property.getReferenceId());
+      }
     }
 
     List<Property> updatedProperties = jobContext.getUpdatedProperties();
+
+    int updatedSuccessfully = 0;
     log.info("Writing {} updated properties for organization: {} - ({})", updatedProperties.size(),
         organization.getName(), organization.getEntityId());
     for (Property property : updatedProperties) {
-      propertiesService.save(token.getIdToken(), property);
+      try{
+        propertiesService.save(token.getIdToken(), property);
+        updatedSuccessfully++;
+      } catch (Exception e) {
+        log.error("Failed to save updates for property with referenceId {}",
+            property.getReferenceId());
+        failedIds.add(property.getReferenceId());
+      }
+      stepStatistics.put(CREATED, successfullyCreated);
+      stepStatistics.put(UPDATED, updatedSuccessfully);
+      stepStatistics.put(FAILED, FAILED_IDS.length());
+      stepStatistics.put(FAILED_IDS, FAILED_IDS);
+
+      jobContext.getJobStatistics().put(STEP_NAME, stepStatistics);
     }
     return RepeatStatus.FINISHED;
   }
